ca5244756fc2acd96cca1e6cc7dfecba1c7f10cd
G6VQU4 <ria8246@gmail.com>
2017-02-15 13:50:47 +0100

    Karakter összehasonlítások száma kiíratásokkal

--- src/main.java
+++ src/main.java
@@ -6,7 +6,8 @@ class HelloWorldApp {
         //String str = "AABBBCCCCDD";
         String str = null;
         if (args.length == 0) {
-            str = "AABBBCCCCDDXDDDDDNJKLLLLLL";
+            ///str = "AABBBCCCCDDXDDDDDNJKLLLLLL";
+            str = "AABBBCCCCDD";
         }else {
             str = args[0];
         }
@@ -18,13 +19,25 @@ class HelloWorldApp {
         int leghosszabb_max = 0;
         int tempLeghosszabb_num = 0;
 
+        int counter =0;
 
         while (i < str.length()) {
             tempLeghosszabb_ch = str.toUpperCase().charAt(i);
             tempLeghosszabb_num =0;
+
+            System.out.println("[i="+ i + "]");
+            System.out.println("[tempLeghosszabb_ch=" + tempLeghosszabb_ch + "]");
+
             while (j < str.length() && tempLeghosszabb_ch == str.toUpperCase().charAt(j)) {
+                System.out.println("[[i="+ i + "]]");
+                System.out.println("[[j="+ j + "]]");
+                System.out.println("[[tempLeghosszabb_ch=" + tempLeghosszabb_ch + "]]");
+                System.out.println("[[str.toUpperCase().charAt(j)="+ str.toUpperCase().charAt(j) + "]]");
+                System.out.println("==========");
+
                 j++;
                 tempLeghosszabb_num++;
+                counter++;
             }
 
             if(tempLeghosszabb_num >= leghosszabb_max){
@@ -32,11 +45,15 @@ class HelloWorldApp {
                 leggyakoribb_ch = tempLeghosszabb_ch ;
             }
             i++;
+            counter++;
         }
 
-        System.out.println("Karakter összehasonlítások száma: " + (j-1));
+        System.out.println("Karakter összehasonlítások száma_: " + i);
         System.out.println("leghosszabb_max: " + leghosszabb_max);
         System.out.println("leghosszabb: " + leggyakoribb_ch);
+        System.out.println("j="+ j);
+        System.out.println("i="+ i);
+        System.out.println("counter="+ counter);
 
     }
 }
